d51f8d8c111c394ef43547d5cc4a0d1bd89dcc8e
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-10-16 21:29:23 -0700

    Initial Commit

---
+++ Date Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Date Class
+//
+//  Created by Amanpreeet Sandhu on 2024-10-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
